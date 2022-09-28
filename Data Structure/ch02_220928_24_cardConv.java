197938a5c51fbdc5fc305d0dd15d3ded93bb41c6
jindol <parkmoc21@gmail.com>
2022-09-28 16:18:15 +0900

    new file: ch02_220928_28_primeNum.java 외 2건
    2022년 9월 28일 16시 19분 자료구조실습 학습파일 커밋
    Web 2.0 과제 수정본 커밋

---
+++ Data Structure/ch02_220928_24_cardConv.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+// class CardConv {
+//     static int cardConv(int x, int r, char[] d) {
+//         int digits = 0;
+//         String dchar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+//         do {
+//             d[digits++] = dchar.charAt(x % r);
+//             x /= r;
+//         } while (x != 0);
+
+//         for (int i = 0; i < digits / 2; i++) {
+//             char t = d[i];
+//             d[i] = d[digits - i - 1];
+//             d[digits - i - 1] = t;
+//         }
+//         return digits;
+//     }
+// }
+
+public class ch02_220928_24_cardConv { 
+    public static int cardConv(int x, int r, char[] d) {
+        int digits = 0;
+        String dchar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        do {
+            d[digits++] = dchar.charAt(x % r);
+            x /= r;
+        } while (x != 0);
+
+        for (int i = 0; i < digits / 2; i++) {
+            char t = d[i];
+            d[i] = d[digits - i - 1];
+            d[digits - i - 1] = t;
+        }
+        return digits;
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int no, cd, dno, retry;
+        char[] cno = new char[32];
+
+        System.out.println("10진수를 기수 변환합니다.");
+        do {
+            do{
+                System.out.println("변환하는 음이 아닌 정수");
+                no = stdIn.nextInt();
+            } while (no < 0);
+            // 입력받은 값이 양의 정수면 cd에 넣고, 아니면 계속 반복
+
+            do {
+                System.out.println("어떤 진수로 변환할까요? (2~36): ");
+                cd = stdIn.nextInt();
+            } while (cd < 2 || cd > 36);
+
+            dno = cardConv(no, cd, cno);
+
+            System.out.print(cd + "진수로");
+            for (int i = 0; i < dno; i++)
+                System.out.print(cno[i]);
+            System.out.println("입니다");
+
+            System.out.println("다시 한 번 할까요? (1...예 / 0...아니요): ");
+            retry = stdIn.nextInt();
+        } while (retry == 1);
+
+        stdIn.close();
+    }
+}
---
+++ Data Structure/ch02_220928_28_primeNum.java
@@ -0,0 +1,46 @@
+public class ch02_220928_28_primeNum {
+
+    private static void method03() {
+        int n, i, j;
+        int count = 0;
+        boolean isPrime;
+        int[] prime = new int [500];    // 소수를 저장할 배열
+    int ptr = 0;                        // 저장된 소수의 개수
+
+        prime[ptr++] = 2;
+
+        for (i = 3; i < 1000; i += 2) {
+            isPrime = true;
+            for(j = 0; j < ptr; j++) {
+                count++;
+                if (i % prime[j] == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                prime[ptr++] = i;
+            }
+        }
+        for (i = 0; i < ptr; i++) {
+            System.out.print(prime[i] + ", ");
+        }
+        System.out.println("\n나눗셈 횟수: " + count);
+    }
+    public static void main(String[] args) {
+        // 2에서 1000까지의 소수 나열하기
+        // 2에서 1000까지의 바깥 for문 돌기
+        // 안쪽 for 문에서 2부터 (자신의 수 - 1)까지 나누어서 떨어지면 소수가 아님
+        int n;
+        int count = 0;
+        int i, j;
+        for (i = 0; i < 999; i++) {
+            n = i + 2;
+            for (j = 0; j < n-2; j++) {
+                if(n % (j+2) == 0) {
+                    
+                }
+            }
+        }
+    }
+}
