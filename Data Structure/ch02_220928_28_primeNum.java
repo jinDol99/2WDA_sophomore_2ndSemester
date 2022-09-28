6f07c308c0825f42d5fbfcb95f1a2b1b83d8bf3d
jindol <parkmoc21@gmail.com>
2022-09-28 17:51:31 +0900

    modified: ch02_220928_28_primeNum.java
    2022년 9월 28일 17시 51분 자료구조실습 학습파일 중 소수출력 1번 작성 및 공부중...

--- Data Structure/ch02_220928_28_primeNum.java
+++ Data Structure/ch02_220928_28_primeNum.java
@@ -1,4 +1,26 @@
 public class ch02_220928_28_primeNum {
+    private static void primeNum01() {
+        int n, i, j;
+        int count = 0;
+        boolean isPrime;
+
+        for (i = 0; i < 999; i++) {
+            n = i + 2;
+            isPrime = true;
+            for (j = 0; j < (n-2); j++) {
+                count++;
+                if(n % (j+2) == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime == true) {
+                System.out.print(n + ", ");
+            }
+        }
+        System.out.println("\n나눗셈 횟수: " + count);
+    }
+
 
     private static void method03() {
         int n, i, j;
@@ -28,19 +50,6 @@ public class ch02_220928_28_primeNum {
         System.out.println("\n나눗셈 횟수: " + count);
     }
     public static void main(String[] args) {
-        // 2에서 1000까지의 소수 나열하기
-        // 2에서 1000까지의 바깥 for문 돌기
-        // 안쪽 for 문에서 2부터 (자신의 수 - 1)까지 나누어서 떨어지면 소수가 아님
-        int n;
-        int count = 0;
-        int i, j;
-        for (i = 0; i < 999; i++) {
-            n = i + 2;
-            for (j = 0; j < n-2; j++) {
-                if(n % (j+2) == 0) {
-                    
-                }
-            }
-        }
+        primeNum01();
     }
 }
