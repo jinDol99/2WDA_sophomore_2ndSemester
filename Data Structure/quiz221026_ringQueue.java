a5b72965e701c3df2ffe56b5b916636a4d3022e6
jindol <parkmoc21@gmail.com>
2022-10-28 14:14:42 +0900

    modified: quiz221026_ringQueue.java
    2022년 10월 28일 16:15 자료구조 실습 과제 중간 저장

--- Data Structure/quiz221026_ringQueue.java
+++ Data Structure/quiz221026_ringQueue.java
@@ -7,30 +7,6 @@ class IntArrayQueue {
     private int num;        // 현재 데이터 개수
     private int front;      // 맨 처음 요소 커서
     private int rear;       // 맨 끝 요소 커서. 즉, 새로운 요소가 들어갈 인덱스 값
-
-    public class EmptyIntQueueException extends RuntimeException {
-        public EmptyIntQueueException() {
-           System.out.println("empty 발생");
-        }
-    }
-
-    public class OverflowIntQueueException extends RuntimeException {
-        public OverflowIntQueueException() {
-           System.out.println("full 발생");
-        }
-    }
-
-    public class OverflowIntStackException extends RuntimeException {
-        public OverflowIntStackException() {
-           System.out.println("full 발생");
-        }
-    }
-
-    public class EmptyIntArrayQueueException extends RuntimeException {
-        public EmptyIntArrayQueueException() {
-           System.out.println("empty 발생");
-        }
-    }
     
     //  --- 생성자 --- //
     public IntArrayQueue(int maxlen) {
@@ -40,32 +16,39 @@ class IntArrayQueue {
             que = new int[capacity];
         } catch (OutOfMemoryError e) {              // 생성이 불가능한 에러를 발견할 경우
             capacity = 0;                           // 큐의 용량을 0으로 설정하여 저장공간을 사용 불가능하게 만듦.
+        } finally {
+            System.out.println("배열의 개수가 " + capacity + "개인 큐 선언 완료");
         }
     }
 
-    public int enque(int x) throws OverflowIntStackException {
+    public int enque(int x) {
         if (num >= capacity)
-            throw new OverflowIntStackException();
-        que[rear++] = x;
-        num++;
-        if(rear == capacity)
-            rear = 0;
+            System.out.println("현재 배열이 꽉 찼습니다.");
+        else {
+            que[rear++] = x;
+            num++;
+            if(rear == capacity)
+                rear = 0;
+        }
         return x;
     }
 
-    public int deque() throws EmptyIntQueueException {
+    public int deque() {
+        int x = 0;
         if (num <= 0)
-            throw new EmptyIntQueueException();
-        int x = que[front++];
-        num--;
+            System.out.println("현재 배열이 비어있습니다.");
+        else {
+            x = que[front++];
+            num--;
         if (front == capacity)
             front = 0;
+        }
         return x;
     }
 
-    public int peek() throws EmptyIntArrayQueueException {
+    public int peek() {
         if (num <= 0)
-            throw new EmptyIntArrayQueueException();
+            System.out.println("현재 배열이 비어있습니다.");
         return que[num - 1];
     }
 
@@ -103,8 +86,7 @@ class IntArrayQueue {
 public class quiz221026_ringQueue {
     public static void main(String[] args) {
         IntArrayQueue ringArr = new IntArrayQueue(20);
-        ringArr.enque(15);
+        ringArr.enque(5);
         ringArr.getQueueStat();
-
     }
 }
