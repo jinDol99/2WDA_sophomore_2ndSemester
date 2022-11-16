ffc489e808cb3fdc9bc70064327769fa06fffdea
jindol <parkmoc21@gmail.com>
2022-11-16 09:03:54 +0900

    new file: dynamicWebCrawling221115.py
    22.11.16 09:04 야간특강 동적웹 크롤링 실습

---
+++ Night Lecture/ch06_221115/ch06221115.py
@@ -0,0 +1,2 @@
+from selenium import webdriverUsers
+wd = webdriver.Chrome('C:/Users/parkm/OneDrive/공부/Github/2WDA_sophomore_2ndSemester_DESKTOP/Night Lecture/ch06_221115/WebDriver/chromedriver.exe')
