47afc5edf6216e6ca4937432655f8dc4ba7f71b5
jindol <parkmoc21@gmail.com>
2022-11-21 10:52:51 +0900

    new file: header.vue / modified: App.vue 외 1개
    2022.11.21 10:53 Vue 개인 프로젝트
    새로운 header 컴포넌트 생성 및 정상 출력 테스트 완료!

---
+++ Web 2.0/portfolio_2022_parkjinseok/portfolio_2022_parkjinseok/src/components/header.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="head">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'header',
+    data() {
+        return {
+            msg: '헤더 TEST'
+        }
+    }
+}
+</script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+    font-weight: normal;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+
+.head {
+    background-color: #04aa6d;
+}
+</style>
+  
