8f88638ccd2326420b0ea596ee83423441418afb
jindol <parkmoc21@gmail.com>
2022-11-21 17:54:13 +0900

    new file: navPage.vue / modified: main.vue
    네비게이션 페이지 생성 후 메인 페이지 연결, 출력 테스트 완료!
    2022.11.21 17:55 Web 2.0 Vue를 이용한 포트폴리오 페이지 제작 프로젝트

--- Web 2.0/portfolio_2022_parkjinseok/portfolio_2022_parkjinseok/src/components/main.vue
+++ Web 2.0/portfolio_2022_parkjinseok/portfolio_2022_parkjinseok/src/components/main.vue
@@ -1,6 +1,9 @@
 <template> 
     <div class="main">
         <headerPage></headerPage>
+        <nav>
+            <navPage></navPage>
+        </nav>
         <main>
             <h1> MAIN 페이지 테스트</h1>
         </main>
@@ -11,12 +14,15 @@
 <script>
 import headerPage from './headerPage.vue';
 import footerPage from './footerPage.vue';
+import navPage from './navPage.vue';
+
 
 export default {
     name: 'main',
     components: {
         headerPage,
         footerPage,
+        navPage,
     },
     data() {
         return {
---
+++ Web 2.0/portfolio_2022_parkjinseok/portfolio_2022_parkjinseok/src/components/navPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="nav">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'navPage',
+    data() {
+        return {
+            msg: '네비게이션 테스트'
+        }
+    }
+}
+</script>
+
+<style>
+.nav {
+    background-color: #00bdbd;
+}
+</style>
